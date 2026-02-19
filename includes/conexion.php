af759751149b5f23eb1da2de39506e6980671ff0
jotacehernandez <yualvili27@gmail.com>
2026-02-18 19:25:50 -0500

    Ocultar warnings deprecated PHP 8.2

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,5 +1,9 @@
 <?php
 
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_NOTICE);
+ini_set('display_errors', 0);
+
+
 $host = "mysql.railway.internal";
 $user = "root";
 $pass = "imbrADBmcQyDmipbdgzLrcJKhGuvWSAK";
